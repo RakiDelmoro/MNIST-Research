6a839ea0dbfc06e1de7ba120d8829de7ec460a39
Raki Delmoro <rakidelmoro@gmail.com>
2024-10-21 13:00:13 +0000

    Update to support torch and cupy based model

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
-from runner import runner
-from model import neural_network
+import torch
+from model_v1.model import neural_network
+from mlp_torch_model.model import MlpNetwork
+from torch.utils.data import DataLoader
 from utils import load_data_to_memory
-from activation_functions import relu
+from torchvision import transforms, datasets
+from nn_utils.activation_functions import leaky_relu
 
 def main():
-    EPOCHS = 50
+    EPOCHS = 10
     BATCH_SIZE = 2098
     IMAGE_WIDTH = 28
     IMAGE_HEIGHT = 28
     LEARNING_RATE = 0.001
     INPUT_DATA_FEATURE_SIZE = IMAGE_HEIGHT*IMAGE_WIDTH
+    LOSS_FUNCTION = torch.nn.CrossEntropyLoss()
+    
+    # Use to transform image for TORCH based model
+    torch_based_transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307, ), (0.3081,))])
+    # Use to transfrom image for CUPY based model
+    cupy_based_transform = transforms.Compose([transforms.Normalize((0.1307, ), (0.3081,))])
 
-    forward_pass, backward_pass, update_parameters, model_parameters = neural_network([INPUT_DATA_FEATURE_SIZE, 1000, 1000, 10], relu)
-    training_tuple_of_arrays, validation_tuple_of_arrays = load_data_to_memory('./training-data/mnist.pkl.gz')
+    training_dataset = datasets.MNIST('./training-data', download=True, train=True, transform=torch_based_transform)
+    validation_dataset = datasets.MNIST('./training-data', download=True, train=False, transform=torch_based_transform)
+    training_dataloader = DataLoader(training_dataset, batch_size=BATCH_SIZE, shuffle=True)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-    runner(EPOCHS, training_tuple_of_arrays, validation_tuple_of_arrays, True, BATCH_SIZE, update_parameters, forward_pass, backward_pass, model_parameters, learning_rate=LEARNING_RATE)
+    # # TORCH Model
+    MlpNetwork().runner(epochs=EPOCHS, training_loader=training_dataloader, validation_loader=validation_dataloader, loss_function=LOSS_FUNCTION,
+                      learning_rate=LEARNING_RATE)
 
+    # CUPY Model
+    runner = neural_network([784, 2000, 2000, 10])
+    runner(EPOCHS, training_dataloader, validation_dataloader, LEARNING_RATE)
 main()
