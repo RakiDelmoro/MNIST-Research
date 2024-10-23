07c49f5a2cd7b08093f591503a42441c93a233ae
Raki Delmoro <rakidelmoro@gmail.com>
2024-10-23 03:52:40 +0000

    Update

--- model_v1/model.py
+++ model_v1/model.py
@@ -1,16 +1,21 @@
 import cupy as cp
-import torch
-from torch.nn.functional import one_hot
+from cupy_utils.utils import one_hot
+from nn_utils.activation_functions import leaky_relu, softmax
 
 def neural_network(network_size: list):
-    axons = [cp.random.randn(network_size[size], network_size[size+1]) for size in range(len(network_size)-1)]
-    dentrites = [cp.random.randn(network_size[size+1]) for size in range(len(network_size)-1)]
+    axons = [cp.random.randn(network_size[size], network_size[size+1]) * cp.sqrt(2/network_size[size]) for size in range(len(network_size)-1)]
+    dentrites = [cp.zeros(network_size[size+1]) for size in range(len(network_size)-1)]
 
     def forward_in_neurons(input_neurons):
         previous_neurons = input_neurons
         neurons_activations = [input_neurons]
         for neurons_layer_index in range(len(network_size)-1):
-            neurons_activation = cp.dot(previous_neurons, axons[neurons_layer_index]) + dentrites[neurons_layer_index]
+            last_layer_idx = len(network_size)-2
+            if neurons_layer_index != last_layer_idx:
+                neurons_activation = leaky_relu(cp.dot(previous_neurons, axons[neurons_layer_index]) + dentrites[neurons_layer_index])
+            else:
+                neurons_activation = softmax(cp.dot(previous_neurons, axons[neurons_layer_index]) + dentrites[neurons_layer_index])
+
             neurons_activations.append(neurons_activation)    
             previous_neurons = neurons_activation
 
@@ -20,24 +25,19 @@ def neural_network(network_size: list):
         previous_neurons = input_neurons
         neurons_activations = [input_neurons]
         for neurons_layer_index in range(len(network_size)-2):
-            neurons_activation = cp.dot(previous_neurons, axons[-(neurons_layer_index+1)].transpose()) + dentrites[-(neurons_layer_index+2)]
+            neurons_activation = leaky_relu(cp.dot(previous_neurons, axons[-(neurons_layer_index+1)].transpose()) + dentrites[-(neurons_layer_index+2)])
             neurons_activations.append(neurons_activation)
             previous_neurons = neurons_activation
 
         return neurons_activations
-    
+
     def calculate_network_stress(forward_pass_activations, backward_pass_activations):
-        neurons_losses = []
         neurons_loss_and_previous_neurons = []
         for each_neurons_activation in range(len(network_size)-1):
             neurons_loss = forward_pass_activations[each_neurons_activation+1] - backward_pass_activations[-(each_neurons_activation+1)]
             previous_neurons = forward_pass_activations[each_neurons_activation]
-
-            neurons_losses.append(neurons_loss)
             neurons_loss_and_previous_neurons.append((previous_neurons, neurons_loss))
-        network_stress = sum(neurons_loss)**2
-        
-        return neurons_loss_and_previous_neurons, cp.mean(network_stress).item()
+        return neurons_loss_and_previous_neurons, neurons_loss
     
     def update_axons_and_dentrites(neurons_loss_and_previous_neurons, learning_rate):
         for neurons_index, (each_neurons_axons, each_neurons_dentrites) in enumerate(zip(axons, dentrites)):
@@ -48,26 +48,26 @@ def neural_network(network_size: list):
             each_neurons_dentrites -= learning_rate * cp.sum(neurons_loss, axis=0)
     
     def training_run(training_loader, learning_rate):
-        batched_network_losses = []
+        batched_network_stress = []
         for input_batch, expected_batch in training_loader:
             input_batch = cp.array(input_batch).reshape(input_batch.shape[0], -1)
-            expected_batch = cp.array(one_hot(expected_batch, num_classes=10))
+            expected_batch = one_hot(cupy_array=expected_batch, number_of_classes=10)
             forward_neurons_activations = forward_in_neurons(input_batch)
             backward_neurons_activations = backward_in_neurons(expected_batch)
 
             # Update parameters
             neurons_loss_correspond_to_previous_neurons, network_stress = calculate_network_stress(forward_neurons_activations, backward_neurons_activations)
             update_axons_and_dentrites(neurons_loss_correspond_to_previous_neurons, learning_rate)
-            batched_network_losses.append(network_stress)
+            batched_network_stress.append(network_stress)
 
-        return batched_network_losses
+        return batched_network_stress
 
     def test_run(validation_dataloader):
         test_loss = 0
         correct = 0
         for input_batch, expected_batch in validation_dataloader:
-            input_batch = cp.array(input_batch)
-            expected_batch = cp.array(expected_batch)
+            input_batch = cp.array(input_batch).reshape(input_batch.shape[0], -1)
+            expected_batch = one_hot(cupy_array=expected_batch, number_of_classes=10)
             model_output = forward_in_neurons(input_batch)[-1]
             test_loss += (model_output - expected_batch).item()
             model_prediction = model_output.argmax(dim=1, keepdim=True)
