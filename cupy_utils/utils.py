b9bbf99c2d7e8b1753f985f9c2d1869fe49b32ae
Raki Delmoro <rakidelmoro@gmail.com>
2024-10-23 09:41:09 +0000

    Implement weight and bias initialization in cupy.

--- cupy_utils/utils.py
+++ cupy_utils/utils.py
@@ -1,4 +1,13 @@
 import cupy as cp
 
 def one_hot(cupy_array, number_of_classes):
-    return cp.eye(number_of_classes)[cupy_array]
+    return cp.array(cp.eye(number_of_classes)[cupy_array], dtype=cp.float32)
+
+def axons_and_dentrites_initialization(input_feature, output_feature):
+    bound_w = cp.sqrt(3) * cp.sqrt(5) / cp.sqrt(input_feature) if input_feature > 0 else 0
+    bound_b = 1 / cp.sqrt(output_feature) if output_feature > 0 else 0
+    weights = cp.random.uniform(-bound_w, bound_w, size=(input_feature, output_feature))
+    bias = cp.random.uniform(-bound_b, bound_b, size=(output_feature,))
+
+    return weights, bias
+
