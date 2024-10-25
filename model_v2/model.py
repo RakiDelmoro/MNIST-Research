0fdce374fd9a0b6e0f424df7d42004990b9db4b5
Raki Delmoro <rakidelmoro@gmail.com>
2024-10-25 13:18:05 +0000

    Array for neurons activation instead of list.

--- model_v2/model.py
+++ model_v2/model.py
@@ -1,79 +1,68 @@
 import torch
 import cupy as cp
+from cupy_utils.utils import one_hot
 from features import GREEN, RED, RESET
-from nn_utils.activation_functions import leaky_relu, softmax
 from nn_utils.loss_functions import cross_entropy_loss
-from cupy_utils.utils import axons_initialization, dentrites_initialization, one_hot
+from nn_utils.activation_functions import leaky_relu, softmax
+from model_v2.utils import forward_pass_architecture, backward_pass_architecture, get_non_zero_value
 
 def neural_network(network_architecture: list):
-    forward_layers_axons = []
-    forward_layers_dentrites = []
-    for feature_size_idx in range(len(network_architecture)-1):
-        forward_axons = axons_initialization(network_architecture[feature_size_idx], network_architecture[feature_size_idx+1])
-        forward_dentrites = dentrites_initialization(network_architecture[feature_size_idx+1]) 
-        forward_layers_axons.append(forward_axons)
-        forward_layers_dentrites.append(forward_dentrites)
-
-    backward_layers_axons = []
-    backward_layers_dentrites = []
-    reverse_network_architecture = network_architecture[::-1]
-    for feature_size_idx in range(len(reverse_network_architecture)-1):
-        backward_axons = axons_initialization(reverse_network_architecture[feature_size_idx], reverse_network_architecture[feature_size_idx+1])
-        backward_dentrites = dentrites_initialization(network_architecture[feature_size_idx+1])
-        backward_layers_axons.append(backward_axons)
-        backward_layers_dentrites.append(backward_dentrites)
+    #TODO: Create a function for network architecture layers
+    # Return Tuple of axons and dentrites
+    # axons is an array shape -> (total connections, filled value until to a certain feature size, filled value until to a certain feature size)
+    # dentrites is an array shape -> (total connection, filled value until to a certain feature size)
+    forward_pass_axons_and_dentrites = forward_pass_architecture(network_features_size=network_architecture)
+    backward_pass_axons_and_dentrites = backward_pass_architecture(network_features_size=network_architecture[::-1])
 
     def forward_in_neurons(neurons):
-        neurons = cp.array(neurons.reshape(neurons.shape[0], -1))
-        neurons_activations = [neurons]
+        neurons = cp.array(neurons)
+        # Shape -> (Total activations, batch_size, maximum value in network_architecture)
+        neurons_activations = cp.zeros(shape=(len(network_architecture),  neurons.shape[0], max(network_architecture)))
+        neurons_activations[0, :, :neurons.shape[-1]] = neurons
         for neurons_layer_idx in range(len(network_architecture)-1):
-            forward_last_layer = len(network_architecture)-2 == neurons_layer_idx
-            neurons = cp.dot(neurons, forward_layers_axons[neurons_layer_idx])
-            # if not forward_last_layer:
-            #     neurons = leaky_relu(cp.dot(neurons, forward_layers_axons[neurons_layer_idx]))
-            # else:
-            #     neurons = softmax(cp.dot(neurons, forward_layers_axons[neurons_layer_idx]))
-            neurons_activations.append(neurons)
+            axons = get_non_zero_value(forward_pass_axons_and_dentrites[0][neurons_layer_idx])
+            neurons = cp.dot(neurons, axons)
+            neurons_activations[neurons_layer_idx+1, :, :neurons.shape[-1]] = neurons
         return neurons_activations
 
     def backward_in_neurons(neurons):
-        neurons = one_hot(cp.array(neurons), number_of_classes=10)
-        neurons_activations = [neurons]
+        neurons = cp.array(neurons)
+        neurons_activations = cp.zeros(shape=(len(network_architecture), neurons.shape[0], max(network_architecture)))
+        neurons_activations[0, :, :neurons.shape[-1]] = neurons
         for neurons_layer_idx in range(len(network_architecture)-1):
-            backward_last_layer = len(reverse_network_architecture)-2 == neurons_layer_idx
-            neurons = cp.dot(neurons, backward_layers_axons[neurons_layer_idx])
-            # if not backward_last_layer:
-            #     neurons = leaky_relu(cp.dot(neurons, backward_layers_axons[neurons_layer_idx]))
-
-            # else:
-            #     neurons = softmax(cp.dot(neurons, backward_layers_axons[neurons_layer_idx]))
-            neurons_activations.append(neurons)
+            axons = get_non_zero_value(backward_pass_axons_and_dentrites[0][neurons_layer_idx])
+            neurons = cp.dot(neurons, axons)
+            neurons_activations[neurons_layer_idx+1, :, :neurons.shape[-1]] = neurons
         return neurons_activations
 
-    def calculate_network_stress(forward_activations, backward_activations, loss_function):
+    def calculate_network_stress(forward_activations, backward_activations):
+        #TODO: Do not derive average here do it in print statement
         layers_neurons_stress = []
         each_layer_avg_stress = []
         for activation_idx in range(len(network_architecture)):
+            # Calculate stress start from forward pass output and backward pass input
             neurons_stress = forward_activations[-(activation_idx+1)] - backward_activations[activation_idx]
-
             layers_neurons_stress.append(neurons_stress)
             each_layer_avg_stress.append(cp.mean(neurons_stress))
         return layers_neurons_stress, cp.mean(cp.array(each_layer_avg_stress)).item()
 
     def update_axons_and_dentrites_forward_layers(layers_stress, neurons_activations, learning_rate):
-        for layer_neurons_idx, (layer_axons, layer_dentrites) in enumerate(zip(forward_layers_axons[::-1], forward_layers_dentrites)):
+        for layer_neurons_idx in range(len(forward_pass_axons_and_dentrites)):
             layer_neurons_activation = neurons_activations[-(layer_neurons_idx+2)]
             layer_stress = layers_stress[layer_neurons_idx]
 
-            layer_axons -= learning_rate * cp.dot(layer_neurons_activation.transpose(), layer_stress)
-            # layer_dentrites -= learning_rate * cp.sum(layer_stress, axis=0)
+            forward_pass_axons_and_dentrites[-(layer_neurons_idx+1)] -= learning_rate * cp.dot(layer_neurons_activation.transpose(), layer_stress)
 
     def update_axons_and_dentrites_backward_layers(layers_stress, neurons_activations, learning_rate):
-        for layer_neurons_idx, (layer_axons, layer_dentrites) in enumerate(zip(backward_layers_axons[::-1], backward_layers_dentrites)):
-            layer_neurons_activation = neurons_activations[-(layer_neurons_idx+1)]
-            layer_stress = layers_stress[-(layer_neurons_idx+2)]
-            layer_axons = layer_axons.transpose()
-            layer_axons -= learning_rate * cp.dot(layer_neurons_activation.transpose(), layer_stress)
+        for layer_neurons_idx in range(len(backward_layers_axons)): #TODO: Fix
+            # layer_axons = 
+            layer_neurons_activation = neurons_activations[-(layer_neurons_idx+2)]
+            layer_stress = layers_stress[-(layer_neurons_idx+1)]
+
+            backward_layers_axons[-(layer_neurons_idx+1)] += learning_rate * cp.dot(layer_neurons_activation.transpose(), layer_stress) #TODO: FIx
+        # for layer_neurons_idx, (layer_axons, layer_dentrites) in enumerate(zip(backward_layers_axons[::-1], backward_layers_dentrites)):
+        #     layer_neurons_activation = neurons_activations[-(layer_neurons_idx+2)]
+        #     layer_stress = layers_stress[-(layer_neurons_idx+1)]
             # layer_dentrites += learning_rate * cp.sum(layer_stress, axis=0)
 
     def training_run(training_loader, loss_function, learning_rate):
@@ -82,11 +71,12 @@ def neural_network(network_architecture: list):
             forward_activations = forward_in_neurons(input_batch)
             backward_activations = backward_in_neurons(expected_batch)
             neurons_stress, average_layer_stress = calculate_network_stress(forward_activations, backward_activations, loss_function)
+            print(average_layer_stress)
             # Update bidirectional passes parameters
             update_axons_and_dentrites_forward_layers(neurons_stress, forward_activations, learning_rate)
             update_axons_and_dentrites_backward_layers(neurons_stress, backward_activations, learning_rate)
             per_batch_stress.append(average_layer_stress)
-        
+
         return cp.mean(cp.array(per_batch_stress))
 
     def test_run(dataloader):
@@ -128,4 +118,5 @@ def neural_network(network_architecture: list):
             accuracy = test_run(validation_loader)
             print(f"EPOCH: {epoch+1} Training Loss: {training_loss} Model Accuracy: {accuracy}")
 
+    #TODO: This function will not return otherwise it will run as we call it
     return runner
