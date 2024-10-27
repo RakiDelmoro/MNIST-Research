c6a35d63878ddeca5da368a1973e5181ed998850
Raki Delmoro <rakidelmoro@gmail.com>
2024-10-27 12:48:18 +0000

    Apply dentrites in calculation.

--- model_v2/utils_py_list_format.py
+++ model_v2/utils_py_list_format.py
@@ -2,15 +2,15 @@ import cupy as cp
 from torch.nn.functional import one_hot
 
 def axons_initialization(input_feature, output_feature):
-    # bound_w = cp.sqrt(3) * cp.sqrt(5) / cp.sqrt(input_feature) if input_feature > 0 else 0
-    # weights = cp.random.uniform(-bound_w, bound_w, size=(input_feature, output_feature))
-    weights = cp.random.randn(input_feature, output_feature)
+    bound_w = cp.sqrt(3) * cp.sqrt(5) / cp.sqrt(input_feature) if input_feature > 0 else 0
+    weights = cp.random.uniform(-bound_w, bound_w, size=(input_feature, output_feature))
+    # weights = cp.random.randn(input_feature, output_feature)
     return weights
 
 def dentrites_initialization(output_feature):
-    bias = cp.random.randn(output_feature)
-    # bound_b = 1 / cp.sqrt(output_feature) if output_feature > 0 else 0
-    # bias = cp.random.uniform(-bound_b, bound_b, size=(output_feature,))
+    # bias = cp.random.randn(output_feature)
+    bound_b = 1 / cp.sqrt(output_feature) if output_feature > 0 else 0
+    bias = cp.random.uniform(-bound_b, bound_b, size=(output_feature,))
     return bias
 
 def forward_pass_architecture(network_features_size: list):
@@ -40,7 +40,7 @@ def get_network_activations(input_neurons, network_architecture, network_axons_a
     for neurons_layer_idx in range(len(network_architecture)-1):
         axons = network_axons_and_dentrites[0][neurons_layer_idx]
         dentrites = network_axons_and_dentrites[-1][neurons_layer_idx]
-        neurons = cp.dot(neurons, axons)
+        neurons = cp.dot(neurons, axons) + dentrites
         neurons_activations.append(neurons)
     return neurons_activations
 
@@ -56,24 +56,26 @@ def layers_of_neurons_stress(network_architecture, forward_pass_activations, bac
 def nudge_axons_and_dentrites(layers_stress, neurons_activations, axons_and_dentrites, for_backward_pass, learning_rate):
     layers_axons = axons_and_dentrites[0]
     layers_dentrites = axons_and_dentrites[-1]
-    for layer_connection in range(len(layers_axons)):
+    for layer_connection_idx in range(len(layers_axons)):
         if not for_backward_pass:
-            layer_neuron_activation = neurons_activations[layer_connection]
-            layer_stress = layers_stress[layer_connection+1]
-            layer_axons = layers_axons[layer_connection]
+            layer_neuron_activation = neurons_activations[layer_connection_idx]
+            layer_stress = layers_stress[layer_connection_idx+1]
+            layer_axons = layers_axons[layer_connection_idx]
+            layer_dentrites = layers_dentrites[layer_connection_idx]
             # Problem shape mismatch:
             # layer_axons shape -> (input_feature, output_feature)
             # learning_rate * (cp.dot(layer_stress, layer_axons.transpose() / cp.sum(layer_axons))) -> (batch_size, input_feature)
             #TODO: create a function so that this will work -> layer_axons -= learning_rate * (cp.dot(layer_stress, layer_axons.transpose() / cp.sum(layer_axons))) -> (batch_size, input_feature)
             # layer_axons -= learning_rate * (cp.dot(layer_stress, layer_axons.transpose() / cp.sum(layer_axons)))
             layer_axons -= learning_rate * (cp.dot(layer_neuron_activation.transpose(), layer_stress) / cp.sum(layer_neuron_activation))
-            # dentrites[layer_connection] -= learning_rate * cp.sum(layer_stress, axis=0)
+            layer_dentrites -= learning_rate * cp.sum(layer_stress, axis=0)
         else:
-            layer_neuron_activation = neurons_activations[layer_connection]
-            layer_stress = layers_stress[-(layer_connection+2)]
-            layer_axons = layers_axons[layer_connection]
+            layer_neuron_activation = neurons_activations[layer_connection_idx]
+            layer_stress = layers_stress[-(layer_connection_idx+2)]
+            layer_axons = layers_axons[layer_connection_idx]
+            layer_dentrites = layers_dentrites[layer_connection_idx]
             layer_axons += learning_rate * (cp.dot(layer_neuron_activation.transpose(), layer_stress) / cp.sum(layer_neuron_activation))
-            # dentrites[layer_connection] -= learning_rate * cp.sum(layer_stress, axis=0)
+            layer_dentrites += learning_rate * cp.sum(layer_stress, axis=0)
 
 def test_run_result(dataloader, forward_in_neurons):
     per_batch_accuracy = []
