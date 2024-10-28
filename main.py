9ef8265d0a438d99f5cefb21bc5c7f83f8040384
Raki Delmoro <rakidelmoro@gmail.com>
2024-10-28 13:26:50 +0000

    Update

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from utils import load_data_to_memory
 from torchvision import transforms, datasets 
 from nn_utils.activation_functions import leaky_relu
 
+import matplotlib.pyplot as plt
+
 def main():
     EPOCHS = 10
     BATCH_SIZE = 2098
@@ -18,19 +20,17 @@ def main():
     INPUT_DATA_FEATURE_SIZE = IMAGE_HEIGHT*IMAGE_WIDTH
     NETWORK_ARCHITECTURE = [INPUT_DATA_FEATURE_SIZE, 2000, 2000, NUMBER_OF_CLASSES]
     LOSS_FUNCTION = torch.nn.CrossEntropyLoss()
-    TRANSFORM = lambda x: torch.flatten(transforms.PILToTensor()(x))#transforms.Compose([transforms.ToTensor()], torch.flatten)
+    TRANSFORM = lambda x: torch.flatten(transforms.ToTensor()(x))#transforms.Compose([transforms.ToTensor()], torch.flatten)
     TARGET_TRANSFORM = lambda x: torch.tensor(one_hot(x, number_of_classes=NUMBER_OF_CLASSES), dtype=torch.float32)
 
     training_dataset = datasets.MNIST('./training-data', download=True, train=True, transform=TRANSFORM, target_transform=TARGET_TRANSFORM)
     validation_dataset = datasets.MNIST('./training-data', download=True, train=False, transform=TRANSFORM, target_transform=TARGET_TRANSFORM)
     training_dataloader = DataLoader(training_dataset, batch_size=BATCH_SIZE, shuffle=True)
     validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
-
     # # # # TORCH Model
     # MlpNetwork(network_architecture=NETWORK_ARCHITECTURE).runner(epochs=EPOCHS, training_loader=training_dataloader, validation_loader=validation_dataloader, loss_function=LOSS_FUNCTION,
-    #                   learning_rate=LEARNING_RATE)
+    #            learning_rate=LEARNING_RATE)
 
     # CUPY Model
     neural_network(network_architecture=NETWORK_ARCHITECTURE, training_dataloader=training_dataloader, validation_dataloader=validation_dataloader, learning_rate=LEARNING_RATE, epochs=EPOCHS)
-    # runner(EPOCHS, training_dataloader, validation_dataloader, LOSS_FUNCTION, LEARNING_RATE)
 main()
