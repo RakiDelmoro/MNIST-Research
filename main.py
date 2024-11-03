0c062cb4e199fbc83de805bf61c0bb532b1ff210
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-03 10:46:35 +0000

    Update

--- main.py
+++ main.py
@@ -11,10 +11,12 @@ def main():
     BATCH_SIZE = 2098
     IMAGE_WIDTH = 28
     IMAGE_HEIGHT = 28
-    LEARNING_RATE = 0.001
+    LEARNING_RATE = 0.01
     NUMBER_OF_CLASSES = 10
     INPUT_DATA_FEATURE_SIZE = IMAGE_HEIGHT*IMAGE_WIDTH
-    NETWORK_ARCHITECTURE = [INPUT_DATA_FEATURE_SIZE, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, NUMBER_OF_CLASSES]
+    NETWORK_ARCHITECTURE = [INPUT_DATA_FEATURE_SIZE, 2000, 2000, NUMBER_OF_CLASSES]
+    # NETWORK_ARCHITECTURE = [INPUT_DATA_FEATURE_SIZE, 100, 100, 100, 100, 100, 100,
+    #                         100, 100, 100, 100, 100, 100, 100, 100, 100, NUMBER_OF_CLASSES]
     TRANSFORM = lambda x: torch.flatten(transforms.ToTensor()(x)).type(dtype=torch.float32)
     TARGET_TRANSFORM = lambda x: torch.tensor(one_hot(x, number_of_classes=NUMBER_OF_CLASSES), dtype=torch.float32)
     
@@ -24,7 +26,7 @@ def main():
     validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
     
     # neural_network_v3(NETWORK_ARCHITECTURE, training_dataloader, validation_dataloader, LEARNING_RATE)
-    # neural_network_v2(NETWORK_ARCHITECTURE, training_dataloader, validation_dataloader, LEARNING_RATE, EPOCHS)
-    cupy_mlp_neural_network(NETWORK_ARCHITECTURE, training_dataloader, validation_dataloader, LEARNING_RATE, EPOCHS)
+    neural_network_v2(NETWORK_ARCHITECTURE, training_dataloader, validation_dataloader, LEARNING_RATE, EPOCHS)
+    # cupy_mlp_neural_network(NETWORK_ARCHITECTURE, training_dataloader, validation_dataloader, LEARNING_RATE, EPOCHS)
 
 main()
