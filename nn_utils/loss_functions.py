4b11a1e7703c4d73fd84ed95ae2b79f7c6c30cad
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-03 10:52:36 +0000

    Update

--- nn_utils/loss_functions.py
+++ nn_utils/loss_functions.py
@@ -4,7 +4,7 @@ from nn_utils.activation_functions import softmax
 def cross_entropy_loss(model_prediction, expected):
     # Compute probability distribution of model output
     probability_distribution = softmax(model_prediction)
-    loss = (-cp.sum(expected * cp.log(probability_distribution))) / model_prediction.shape[0]
-    loss_for_backprop = probability_distribution - expected
+    loss = cp.mean(-cp.sum(expected * cp.log(probability_distribution), axis=1))
+    loss_for_backprop = (probability_distribution - expected)
 
     return loss, loss_for_backprop
