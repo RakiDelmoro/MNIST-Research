55c4ac5dd13e38057e75cc617c23fe9bfb08d064
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-09 00:18:41 +0000

    Update

--- utils.py
+++ utils.py
@@ -1,41 +1,11 @@
-import gzip
-import pickle
-import cupy as cp
-import torch
+import random
+from cupy_utils.utils import one_hot
+from Generate_image_digits.digit_generator import digit_to_image
 
-def training(forward_pass, backward_pass, learning_rate):
-    pass
-
-def one_hot_encoded(expected_array):
-    number_of_classes = cp.unique(expected_array).size
-    one_hot_encoded = cp.eye(number_of_classes)[expected_array]
-
-    return torch.tensor(one_hot_encoded, dtype=torch.float32)
-
-def load_data_to_memory(file_name: str):
-    with (gzip.open(file_name, 'rb')) as file:
-        ((training_image_array, training_label_array), (validation_image_array, validation_label_array), _) = pickle.load(file, encoding='latin-1')
-
-    return (torch.tensor(training_image_array, dtype=torch.float32), one_hot_encoded(cp.array(training_label_array))), (cp.array(validation_image_array, dtype=torch.float32), one_hot_encoded(torch.tensor(validation_label_array)))
-
-def cupy_dataloader(samples, batch_size, shuffle):
-    training_samples = samples[0]
-    expected_samples = samples[1]
-
-    total_samples = training_samples.shape[0]
-    number_of_batched_samples = int(cp.ceil(total_samples / batch_size))
-
-    indices = cp.arange(total_samples)
-
-    if shuffle:
-        cp.random.shuffle(indices)
-
-    for index in range(number_of_batched_samples):
-        start_index = index * batch_size
-        end_index = min((index + 1) * batch_size, total_samples)
-        batch_indices = indices[start_index:end_index]
-
-        batched_images = training_samples[batch_indices]
-        batched_expected = expected_samples[batch_indices]
-
-        yield batched_images, batched_expected
+def digit_generator():
+    while True:
+        generated_digit = random.randint(1, 9)
+        one_hot_digit = one_hot(x=generated_digit, number_of_classes=10)
+        pil_image, array_image = digit_to_image(str(generated_digit))
+        pil_image.save("test.png")
+        yield array_image, one_hot_digit
