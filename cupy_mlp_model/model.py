d3913b9f8b90bd6ac0ae1549fedfc6111eb46169
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-12 09:02:19 +0000

    Apply reisdual use boolean

--- cupy_mlp_model/model.py
+++ cupy_mlp_model/model.py
@@ -2,17 +2,25 @@ import cupy as cp
 from features import GREEN, RED, RESET
 from nn_utils.loss_functions import cross_entropy_loss
 from cupy_utils.utils import axons_and_dentrites_initialization
-from cupy_mlp_model.utils import training_layers, test_layers
+from cupy_mlp_model.backprop_utils import training_layers, test_layers
+from cupy_mlp_model.residual_utils import residual_training_layers, residual_test_layers
 
-def cupy_mlp_neural_network(network_feature_sizes, training_loader, validation_loader, learning_rate, epochs):
+def cupy_mlp_neural_network(network_feature_sizes, training_loader, validation_loader, learning_rate, epochs, residual_use):
     network_parameters = [axons_and_dentrites_initialization(network_feature_sizes[feature_idx], network_feature_sizes[feature_idx+1])
                          for feature_idx in range(len(network_feature_sizes)-1)]
-
+    # [1, 2, 4, 8, 16, 32, 64, 128]
+    residual_indexes = [(2**n) for n in range(len(network_parameters)) if 2**n < len(network_parameters)]
     def training_run():
-        return training_layers(dataloader=training_loader, layers_parameters=network_parameters, learning_rate=learning_rate)
+        if residual_use:
+            return residual_training_layers(dataloader=training_loader, layers_parameters=network_parameters, residual_idx=residual_indexes, learning_rate=learning_rate)
+        else:
+            return training_layers(dataloader=training_loader, layers_parameters=network_parameters, learning_rate=learning_rate)
 
     def test_run():
-        return test_layers(dataloader=validation_loader, layers_parameters=network_parameters, total_samples=100)
+        if residual_use:
+            return residual_test_layers(dataloader=validation_loader, layers_parameters=network_parameters, residual_idx=residual_indexes)
+        else:
+            return test_layers(dataloader=validation_loader, layers_parameters=network_parameters)
 
     for epoch in range(epochs):
         print(f'EPOCH: {epoch+1}')
