53f649fc9ca9af8ba2ead21f46be77b6e5d1fc1a
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-17 10:12:59 +0000

    Change middle layers depth

--- oja_runner.py
+++ oja_runner.py
@@ -13,7 +13,7 @@ def main():
     NUMBER_OF_CLASSES = 10
     INPUT_DATA_FEATURE_SIZE = IMAGE_HEIGHT*IMAGE_WIDTH
     INPUT_AND_OUTPUT_LAYERS = [INPUT_DATA_FEATURE_SIZE, NUMBER_OF_CLASSES]
-    MIDDLE_LAYERS = [100] * 100
+    MIDDLE_LAYERS = [100] * 10
     NETWORK_ARCHITECTURE = INPUT_AND_OUTPUT_LAYERS[:1] + MIDDLE_LAYERS + INPUT_AND_OUTPUT_LAYERS[1:]
     TRANSFORM = lambda x: torch.flatten(transforms.ToTensor()(x)).type(dtype=torch.float32)
     TARGET_TRANSFORM = lambda x: torch.tensor(one_hot(x, number_of_classes=NUMBER_OF_CLASSES), dtype=torch.float32)
