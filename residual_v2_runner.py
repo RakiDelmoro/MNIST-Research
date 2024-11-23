bd4bc3e3579d985b99e903e9492811e9cdb56c1a
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-23 06:09:26 +0000

    Normalize image: -1 to 1, deeper middle layers

--- residual_v2_runner.py
+++ residual_v2_runner.py
@@ -15,16 +15,22 @@ def main():
     NUMBER_OF_CLASSES = 10
     INPUT_DATA_FEATURE_SIZE = IMAGE_HEIGHT*IMAGE_WIDTH
     INPUT_AND_OUTPUT_LAYERS = [INPUT_DATA_FEATURE_SIZE, NUMBER_OF_CLASSES]
-    MIDDLE_LAYERS = [512] * 15
+    MIDDLE_LAYERS = [256] * 150
     NETWORK_ARCHITECTURE = INPUT_AND_OUTPUT_LAYERS[:1] + MIDDLE_LAYERS + INPUT_AND_OUTPUT_LAYERS[1:]
     residual_neurons_sizes = [(2**n) for n in range(NETWORK_ARCHITECTURE[1]) if (2**n) < len(MIDDLE_LAYERS)][1:]
-    TRANSFORM = lambda x: torch.flatten(transforms.ToTensor()(x)).type(dtype=torch.float32)
+    # TRANSFORM = lambda x: torch.flatten(transforms.ToTensor()(x)).type(dtype=torch.float32)
+    TRANSFORM = lambda x: torch.flatten(
+        transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.5], std=[0.5])
+        ])(x)
+    ).type(dtype=torch.float32)
     TARGET_TRANSFORM = lambda x: torch.tensor(one_hot(x, number_of_classes=NUMBER_OF_CLASSES), dtype=torch.float32)
     
     training_dataset = datasets.MNIST('./training-data', download=True, train=True, transform=TRANSFORM, target_transform=TARGET_TRANSFORM)
     validation_dataset = datasets.MNIST('./training-data', download=True, train=False, transform=TRANSFORM, target_transform=TARGET_TRANSFORM)
     training_dataloader = DataLoader(training_dataset, batch_size=BATCH_SIZE, shuffle=True)
     validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
-    model(network_architecture=NETWORK_ARCHITECTURE, residual_idx_connections=residual_neurons_sizes, training_loader=digit_generator(), validation_loader=validation_dataloader, learning_rate=LEARNING_RATE, epochs=EPOCHS)
+    model(network_architecture=NETWORK_ARCHITECTURE, residual_idx_connections=residual_neurons_sizes, training_loader=training_dataloader, validation_loader=validation_dataloader, learning_rate=LEARNING_RATE, epochs=EPOCHS)
 
 main()
