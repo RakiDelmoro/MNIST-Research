8a2d92cb97a66d2562f7f6190a6f9f43cb24a1fe
Raki Delmoro <rakidelmoro@gmail.com>
2024-11-25 11:27:38 +0000

    Refactor: remove unused function

--- cupy_utils/utils.py
+++ cupy_utils/utils.py
@@ -9,27 +9,12 @@ def cupy_array(x):
 def one_hot(x, number_of_classes):
     return cupy_array(cp.eye(number_of_classes)[x])
 
-def axons_and_dentrites_initialization(input_feature, output_feature):
+def axons_initialization(input_feature, output_feature):
     weights = torch.empty((input_feature, output_feature))
     bias = torch.empty(output_feature)
     torch.nn.init.kaiming_normal_(weights, a=math.sqrt(5))
-    fan_in, _ = torch.nn.init._calculate_fan_in_and_fan_out(weights)
-    bound = 1 / math.sqrt(fan_in) if fan_in > 0 else 0
-    torch.nn.init.uniform_(bias, -bound, bound)
     # weights = cp.random.normal(loc=0, scale=0.01, size=(input_feature, output_feature))
-    return [cupy_array(weights), cupy_array(bias)]
-
-def axons_initialization(input_feature, output_feature):
-    bound_w = cp.sqrt(5) / cp.sqrt(input_feature) if input_feature > 0 else 0
-    weights = cp.random.uniform(-bound_w, bound_w, size=(input_feature, output_feature), dtype=cp.float32)
-    # weights = cp.random.randn(input_feature, output_feature) * 0.01
-    return weights
-
-def dentrites_initialization(output_feature):
-    bound_b = 1 / cp.sqrt(output_feature) if output_feature > 0 else 0
-    bias = cp.random.uniform(-bound_b, bound_b, size=(output_feature,), dtype=cp.float32)
-    # bias = cp.zeros(output_feature)
-    return 
+    return cupy_array(weights)
 
 def resiudal_connections_initialization(network_feature_sizes):
     network_connections = []
@@ -47,7 +32,7 @@ def resiudal_connections_initialization(network_feature_sizes):
         else:
             input_neurons_size = network_feature_sizes[layer_idx] + residual_neurons_size
             output_neurons_size = network_feature_sizes[layer_idx+1]
-        axons, _ = axons_and_dentrites_initialization(input_neurons_size, output_neurons_size)
+        axons = axons_initialization(input_neurons_size, output_neurons_size)
         network_connections.append(axons)
     return network_connections
 
