3f86e9831eae9bca69c7e7704622acef023e0867
Raki Delmoro <rakidelmoro@gmail.com>
2024-12-18 13:24:54 +0000

    Train more epochs, not one hot encoded label

--- torch_model_runner.py
+++ torch_model_runner.py
@@ -5,7 +5,7 @@ from mlp_torch_model.model import MlpNetwork
 from torchvision import transforms, datasets
 
 def main():
-    EPOCHS = 10
+    EPOCHS = 1000
     BATCH_SIZE = 2098
     IMAGE_WIDTH = 28
     IMAGE_HEIGHT = 28
@@ -13,15 +13,15 @@ def main():
     NUMBER_OF_CLASSES = 10
     LOSS_FUNCTION = torch.nn.CrossEntropyLoss()
     INPUT_DATA_FEATURE_SIZE = IMAGE_HEIGHT*IMAGE_WIDTH
-    NETWORK_ARCHITECTURE = [INPUT_DATA_FEATURE_SIZE,2000, 2000, NUMBER_OF_CLASSES]
+    NETWORK_ARCHITECTURE = [INPUT_DATA_FEATURE_SIZE, 128, NUMBER_OF_CLASSES]
     TRANSFORM = lambda x: torch.flatten(transforms.ToTensor()(x)).type(dtype=torch.float32)
-    TARGET_TRANSFORM = lambda x: torch.tensor(one_hot(x, number_of_classes=NUMBER_OF_CLASSES), dtype=torch.float32)
+    TARGET_TRANSFORM = lambda x: torch.tensor(x)
 
     training_dataset = datasets.MNIST('./training-data', download=True, train=True, transform=TRANSFORM, target_transform=TARGET_TRANSFORM)
     validation_dataset = datasets.MNIST('./training-data', download=True, train=False, transform=TRANSFORM, target_transform=TARGET_TRANSFORM)
     training_dataloader = DataLoader(training_dataset, batch_size=BATCH_SIZE, shuffle=True)
     validation_dataloader = DataLoader(validation_dataset, batch_size=BATCH_SIZE, shuffle=True)
 
-    MlpNetwork(NETWORK_ARCHITECTURE).runner(EPOCHS, training_dataloader, validation_dataloader, LOSS_FUNCTION, LEARNING_RATE)
+    MlpNetwork().runner(EPOCHS, training_dataloader, validation_dataloader, LOSS_FUNCTION, LEARNING_RATE)
 
 main()
