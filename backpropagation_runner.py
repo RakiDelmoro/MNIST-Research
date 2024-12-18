9beaa0b236d1f381577ea05df60f0c48216e9dfe
Raki Delmoro <rakidelmoro@gmail.com>
2024-12-18 13:25:28 +0000

    Torch like model size, not one hot encoded

--- backpropagation_runner.py
+++ backpropagation_runner.py
@@ -12,12 +12,12 @@ def main():
     IMAGE_HEIGHT = 28
     LEARNING_RATE = 0.001
     NUMBER_OF_CLASSES = 10
-    MIDDLE_LAYERS = [2000] * 2
+    MIDDLE_LAYERS = [128] * 2
     INPUT_DATA_FEATURE_SIZE = IMAGE_HEIGHT*IMAGE_WIDTH
     INPUT_AND_OUTPUT_LAYERS = [INPUT_DATA_FEATURE_SIZE, NUMBER_OF_CLASSES]
     NETWORK_ARCHITECTURE = INPUT_AND_OUTPUT_LAYERS[:1] + MIDDLE_LAYERS + INPUT_AND_OUTPUT_LAYERS[1:]
     TRANSFORM = lambda x: torch.flatten(transforms.ToTensor()(x)).type(dtype=torch.float32)
-    TARGET_TRANSFORM = lambda x: torch.tensor(one_hot(x, number_of_classes=NUMBER_OF_CLASSES), dtype=torch.float32)
+    TARGET_TRANSFORM = lambda x: torch.tensor(x)
 
     training_dataset = datasets.MNIST('./training-data', download=True, train=True, transform=TRANSFORM, target_transform=TARGET_TRANSFORM)
     validation_dataset = datasets.MNIST('./training-data', download=True, train=False, transform=TRANSFORM, target_transform=TARGET_TRANSFORM)
